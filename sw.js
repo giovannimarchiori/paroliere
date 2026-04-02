971a6a99f68647a3da101f925599cab03f801df2
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2026-04-02 08:59:30 +0200

    update dice

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "paroliere-v1.0.5"; // cambia versione ad ogni aggiornamento
+const CACHE = "paroliere-v1.0.6"; // cambia versione ad ogni aggiornamento
 
 self.addEventListener("install", e => {
   self.skipWaiting(); // forza aggiornamento
